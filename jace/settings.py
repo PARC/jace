6243952d66353bb096a2cc48f664b5e3cecc5156
Michael Youngblood <Michael.Youngblood@parc.com>
2017-08-07 21:28:01 +0000

    Connecting postgres

--- jace/settings.py
+++ jace/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'jace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jace',
+        'HOST' : os.environ['POSTGRES'].split(':')[0],
+        'PORT' : os.environ['POSTGRES'].split(':')[1],
     }
 }
 
