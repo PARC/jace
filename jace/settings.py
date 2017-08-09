dbeb89b8a0f5288b076d30f674b0763aeeae0fa9
docjoel <joelschooler@me.com>
2017-08-09 08:46:51 -0700

    Update settings.py
    added user_model to apps

--- jace/settings.py
+++ jace/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user_model.apps.user_modelConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
