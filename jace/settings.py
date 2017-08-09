6d610971ec3a949497b9e883143fbbc3e33d25b5
docjoel <joelschooler@me.com>
2017-08-09 08:58:40 -0700

    Update settings.py
    added comma

--- jace/settings.py
+++ jace/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user_model.apps.UserModelConfig'
+    'user_model.apps.UserModelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
