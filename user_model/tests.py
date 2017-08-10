05cf2a817c6cd75d2be898ae691bf9e216ffc4ad
docjoel <joelschooler@me.com>
2017-08-10 09:56:00 -0700

    Added Tests to User_model

--- user_model/tests.py
+++ user_model/tests.py
@@ -17,7 +17,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         non_twoweek= User(identifier="Joel",language="ENG",uuid="12345",timestamp=timezone.now(),deletedIndicator=False,
                           startdate=0,Days_since_start=17,Days_since_last_report=2)
-        is_twoweek =  u = User(identifier="Joel",language="ENG",uuid="12345",timestamp=timezone.now(),deletedIndicator=False,
+        is_twoweek = User(identifier="Joel",language="ENG",uuid="12345",timestamp=timezone.now(),deletedIndicator=False,
                                startdate=0,Days_since_start=14,Days_since_last_report=2)
         self.assertIs(non_twoweek.was_published_recently(), False)
         self.assertIs(non_twoweek.was_published_recently(), True)
