f40df593068d1ac72d5dc85c1ccab64980dbbc7b
docjoel <joelschooler@me.com>
2017-08-10 11:00:59 -0700

    Typos

--- user_model/tests.py
+++ user_model/tests.py
@@ -80,5 +80,6 @@ class QuestionModelTests(TestCase):
     Answered = models.BooleanField()"""
         a = Answer(uuid="9872",timestamp=timezone.now(),question=q,user=u,Answer_text="blah",Answered=True)
 
-        self.assertIs([a,"blah"])
+        self.assertIs([q,"Hello World"])
+        self.assertIs([a, "blah"])
 
