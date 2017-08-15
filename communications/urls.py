c9bd0986b9270f7d967763a0e48571be9a221e7a
docjoel <joelschooler@me.com>
2017-08-15 12:07:08 -0700

    added urls

--- communications/urls.py
+++ communications/urls.py
@@ -1,2 +1,8 @@
+from django.conf.urls import url
+
+from communications import views
+
 urlpatterns = [
+    url(r'^users/$', views.user_list),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
 ]
