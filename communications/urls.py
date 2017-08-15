6f3b107dea2005cb1c3b2fecb4ed680277dc49ab
docjoel <joelschooler@me.com>
2017-08-15 14:25:31 -0700

    urlchanges

--- communications/urls.py
+++ communications/urls.py
@@ -4,8 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from communications import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.user_list.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.user_detail.as_view()),
+    url(r'^users/$', views.user_list.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail.as_view()),
+    url(r'^interventions/$', views.intervention_list.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
