6cefc80b2623ce252c9e9224b1d6d9b8e89632d9
docjoel <joelschooler@me.com>
2017-08-17 10:45:04 -0700

    UUID changes

--- user_model/tests.py
+++ user_model/tests.py
@@ -84,7 +84,7 @@ Answered = models.BooleanField()"""
         :return:s
         """
         self.u.save()
-        self.assertTrue(type(self.u.id) is int)
+        self.assertTrue(type(self.u.uuid) is int)
         self.assertTrue(self.u in User.objects.all())
 
     def test_delete(self):
