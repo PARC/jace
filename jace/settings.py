417c7a256cc1b94dbd12c826b15161b8653a7514
docjoel <joelschooler@me.com>
2017-08-17 08:52:42 -0700

    fixing apk updateline

--- jace/settings.py
+++ jace/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'jace.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jace',
+        'NAME': 'postgres',
         "USER": 'postgres',
         'PASSWORD':"",
         'HOST': os.environ['POSTGRES'].split(':')[0],
