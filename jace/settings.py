5e9316e7c155d83704f6aadd0d29f926de93e4a4
docjoel <joelschooler@me.com>
2017-08-21 11:39:12 -0700

    Added token generations

--- jace/settings.py
+++ jace/settings.py
@@ -40,12 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'communications.apps.CommunicationsConfig',
+    'rest_framework.authtoken',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+    ,
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
--- user_model/updates.py
+++ user_model/updates.py
@@ -1,4 +1,19 @@
 from communications.models import *
 
 for report in FittleReport.objects.all():
-    report.data
+    if report.kind == "Answer":
+        data = report.data
+        event = report.data['event']
+        choices = report.data['choices']
+        answered_on_day = report.data["answeredOnDay"]
+        askDate = report.data["askDate"]
+        askDay = report.data["askDay"]
+        askDateTime = report.data["askDatetime"]
+        askTime = report.data["askTime"]
+        expireDate = report.data['expireDate']
+        expireTime = report.data['expireTime']
+        sequence = report.data['sequence']
+        name = report.data['name']
+        tag = report.data['tag']
+        text = report.data['text']
+        answer = report.data['answer']
