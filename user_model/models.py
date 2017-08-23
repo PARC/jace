7272f851fd3888a6c32e4b9ef953b3ccca9f604d
docjoel <joelschooler@me.com>
2017-08-23 10:37:15 -0700

    added user update

--- user_model/models.py
+++ user_model/models.py
@@ -18,7 +18,7 @@ class User(models.Model):
     UUID = models.CharField(max_length=UUID_FIELD, primary_key=True)
     timestamp = models.DateTimeField()
     deletedIndicator = models.BooleanField()
-    startdate = models.IntegerField()
+    startdate = models.DateTimeField()
     Days_since_start = models.IntegerField()
     Days_since_last_report = models.IntegerField()
 
--- user_model/updates.py
+++ user_model/updates.py
@@ -7,27 +7,30 @@ def update_all():
             try:
                 data = report.data
                 event = report.data['event']
-                choices = report.data['choices']
-                answered_on_day = report.data["answeredOnDay"]
-                askDate = report.data["askDate"]
-                askDay = report.data["askDay"]
-                askDateTime = report.data["askDatetime"]
-                askTime = report.data["askTime"]
-                expireDate = report.data['expireDate']
-                expireTime = report.data['expireTime']
-                sequence = report.data['sequence']
-                name = report.data['name']
-                tag = report.data['tag']
-                text = report.data['text']
-                answer = report.data['answer']
-                createdat = report.data['createdAt']
-                if report.kind == "answer":
-                    if name == "getDisplayName":
-                        u = User(identifier=report.source, language='eng', UUID=report.id,
-                                 timestamp=createdat,
-                                 deletedIndicator=False, startDate=createdat, Days_since_start=0,
-                                 Days_since_last_report=0)
-                        u.save()
+                if report.data["event"] == "answerQuestion":
+                    questionData = data["question"]
+                    choices = questionData["choices"]
+                    answered_on_day = questionData["answeredOnDay"]
+                    askDate = questionData["askDate"]
+                    askDay = questionData["askDay"]
+                    askDateTime = questionData["askDatetime"]
+                    askTime = questionData["askTime"]
+                    expireDate = questionData['expireDate']
+                    expireTime = questionData['expireTime']
+                    sequence = questionData['sequence']
+                    name = questionData['name']
+                    tag = questionData['tag']
+                    text = questionData['text']
+                    answer = questionData['answer']
+                    createdat = questionData['createdAt']
+                    print(name)
+                    if report.kind == "answer":
+                        if name == "getDisplayName":
+                            u = User(identifier=report.source, language='eng', UUID=report.id,
+                                     timestamp=createdat,
+                                     deletedIndicator=False, startdate=createdat, Days_since_start=0,
+                                     Days_since_last_report=0)
+                            u.save()
             except KeyError:
                 data = report.data
                 event = report.data['event']
