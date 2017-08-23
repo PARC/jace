8f9cc03f982b3f3e8839991ff2f91fd21b1f0cce
docjoel <joelschooler@me.com>
2017-08-23 10:51:56 -0700

    added user update

--- user_model/updates.py
+++ user_model/updates.py
@@ -1,36 +1,36 @@
-from communications.models import *
-from user_model.models import *
-
-
-def update_all():
-    for report in debugReport.objects.all():
-        try:
-            data = report.data
-            event = report.data['event']
-            if report.data["event"] == "answerQuestion":
-                questionData = data["question"]
-                choices = questionData["choices"]
-                answered_on_day = questionData["answeredOnDay"]
-                askDate = questionData["askDate"]
-                askDay = questionData["askDay"]
-                askDateTime = questionData["askDatetime"]
-                askTime = questionData["askTime"]
-                expireDate = questionData['expireDate']
-                expireTime = questionData['expireTime']
-                sequence = questionData['sequence']
-                name = questionData['name']
-                tag = questionData['tag']
-                text = questionData['text']
-                answer = questionData['answer']
-                createdat = questionData['createdAt']
-                print(name)
-                if report.kind == "answer":
-                    if name == "getDisplayName":
-                        u = User(identifier=report.source, language='eng', UUID=report.id,
-                                 timestamp=createdat,
-                                 deletedIndicator=False, startdate=0, Days_since_start=0,
-                                 Days_since_last_report=0)
-                        u.save()
-        except KeyError:
-            data = report.data
-            event = report.data['event']
+# from communications.models import *
+# from user_model.models import *
+#
+#
+# def update_all():
+#     for report in debugReport.objects.all():
+#         try:
+#             data = report.data
+#             event = report.data['event']
+#             if report.data["event"] == "answerQuestion":
+#                 questionData = data["question"]
+#                 choices = questionData["choices"]
+#                 answered_on_day = questionData["answeredOnDay"]
+#                 askDate = questionData["askDate"]
+#                 askDay = questionData["askDay"]
+#                 askDateTime = questionData["askDatetime"]
+#                 askTime = questionData["askTime"]
+#                 expireDate = questionData['expireDate']
+#                 expireTime = questionData['expireTime']
+#                 sequence = questionData['sequence']
+#                 name = questionData['name']
+#                 tag = questionData['tag']
+#                 text = questionData['text']
+#                 answer = questionData['answer']
+#                 createdat = questionData['createdAt']
+#                 print(name)
+#                 if report.kind == "answer":
+#                     if name == "getDisplayName":
+#                         u = User(identifier=report.source, language='eng', UUID=report.id,
+#                                  timestamp=createdat,
+#                                  deletedIndicator=False, startdate=0, Days_since_start=0,
+#                                  Days_since_last_report=0)
+#                         u.save()
+#         except KeyError:
+#             data = report.data
+#             event = report.data['event']
