fc93172abb2cb1616473cce1042e65d42be5fae5
docjoel <joelschooler@me.com>
2017-08-28 15:48:01 -0700

    working on redis

--- communications/toRedis.py
+++ communications/toRedis.py
@@ -4,6 +4,11 @@ def post_to_redis(question_dict):
     payload = question_dict
     headers = {'Content-type': 'application/json'}
     r = requests.post('http://localhost:5000/coach/events/20',json=payload,headers=headers)
+
     print(r.json())
     r.close()
 
+
+
+
+post_to_redis({"timeChange":False})
