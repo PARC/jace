64e112f22aec61a605c4690e488dbda3cc67b403
docjoel <joelschooler@me.com>
2017-08-28 13:39:01 -0700

    working on redis

--- statemachine6.py
+++ statemachine6.py
@@ -8,9 +8,8 @@ from communications.outgoing import *
 
 with ruleset('coach'):
 
-
-    @when_all(m.time_to_change)
-    def determine_con():
+    @when_all(m.timeChange == True)
+    def determine_con(c):
         """
         Will activate if it is time for the user to change conditions based on a two week interval
         :return:
@@ -22,8 +21,9 @@ with ruleset('coach'):
         # question_to_server(
         #     '{"studyId":"shszxqlpkw89s98f6","attribute":"settings.implementationIntention", "value":"y{}"}'.format(
         #         random.choice(["yes", "no"])))
-    @when_all(m.NO_responce_number >= 3)
-    def turn_on_reminders():
+        pass
+    @when_all(m.timeChange == False)
+    def turn_on_reminders(c):
         """
         Will activate if the number of lack responces is greater than 3.
         :return:
@@ -32,11 +32,12 @@ with ruleset('coach'):
         # question_to_server(question_to_server(
         #     '{"studyId":"shszxqlpkw89s98f6","attribute":"settings.selfCompassion", "value":"y{}"}'.format(
         #         random.choice(["yes", "no"]))))
+        pass
 
     @when_start
     def start(host):
         for i in range(32):
-            host.post('coach',{"Rx_setup":False})
+            host.post('coach',{"timeChange":False})
 
 if __name__ == '__main__':
     """
@@ -44,3 +45,6 @@ if __name__ == '__main__':
     """
     redis.StrictRedis(host=os.environ['REDIS'].split(':')[0], port=os.environ['REDIS'].split(':')[1]).flushall()
     run_all([{'host': os.environ['REDIS'].split(':')[0], 'port': os.environ['REDIS'].split(':')[1]}]);
+    # redis.StrictRedis(port=32768).flushall()
+    # run_all([{'host': 'localhost', 'port': 32768}], port=32768);
+
