b28718e0a6bc0a311abb893a14c4236b56c93e48
docjoel <joelschooler@me.com>
2017-08-29 11:31:47 -0700

    working on redis

--- test.py
+++ test.py
@@ -28,6 +28,6 @@ with ruleset('animal'):
     def start(host):
         host.assert_fact('animal', { 'subject': 'Kermit', 'predicate': 'eats', 'object': 'flies'})
 
-c = sys.settrace(run_all([{'host': 'localhost', 'port': 32768}]))
+c = sys.settrace(run_all([{'host': 'docker.for.mac.localhost', 'port': 32768}]))
 print(c)
 
