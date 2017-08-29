e77986623d1ca16bb02c9a7ac76efc550c7a7c93
docjoel <joelschooler@me.com>
2017-08-29 10:18:41 -0700

    working on redis

--- communications/toRedis.py
+++ communications/toRedis.py
@@ -3,11 +3,11 @@ import requests
 def post_to_redis(value):
     payload = {"timeChange":value}
     headers = {'Content-type': 'application/json'}
-    r = requests.post('http://localhost:5000/coach/events/20',json=payload,headers=headers)
+    r = requests.post('http://127.0.0.1:5000/coach/events/20',json=payload,headers=headers)
     print(r.json())
     r.close()
 
-
+post_to_redis(False)
 
 
 
