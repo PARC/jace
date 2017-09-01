0cc99ed391eda51f6ae02b0b18b74aebb42a64ac
docjoel <joelschooler@me.com>
2017-09-01 09:09:57 -0700

    deleted interventions reworked survery question builder

--- communications/urls.py
+++ communications/urls.py
@@ -8,13 +8,10 @@ token = "42istheanswer"
 urlpatterns = [
     url(r'^reports/$', views.report_list.as_view()),
     url(r'^users/$', views.user_list.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail.as_view()),
-    url(r'^interventions/(?P<pk>[0-9]+)/$', views.Intervention_detail.as_view()),
     url(r'^surveys/$', views.survey_list.as_view()),
     url(r'^questions/$', views.question_list.as_view()),
-    url(r'^questions/(?P<pk>[0-9]+)/$', views.question_detail.as_view()),
     url(r'^answers/$', views.answer_list.as_view()),
-    url(r'^answers/(?P<pk>[0-9]+)/$', views.answer_detail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
