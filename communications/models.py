439d653f3ea9420860cadf7a157a5e5f9978cbe1
docjoel <joelschooler@me.com>
2017-09-05 11:43:41 -0700

    testing __ID

--- communications/models.py
+++ communications/models.py
@@ -27,11 +27,11 @@ class FittleReport(models.Model):
 
 
 class debugReport(models.Model):
+    _id = models.CharField(max_length=UUID_FIELD)
     studyName = models.CharField(max_length=MEDIUM_LENGTH)
     kind = models.CharField(max_length=MEDIUM_LENGTH)
     data = fields.JSONField()
     source = models.CharField(max_length=SHORT_LENGTH)
-    shared = fields.JSONField()
     def __repr__(self):
         return str(self.data)
 
