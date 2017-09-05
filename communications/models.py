51de618d987a3e9b2de0088892f03b82b689ee81
docjoel <joelschooler@me.com>
2017-09-05 11:24:46 -0700

    testing __ID

--- communications/models.py
+++ communications/models.py
@@ -27,7 +27,7 @@ class FittleReport(models.Model):
 
 
 class debugReport(models.Model):
-    _id = models.CharField(max_length=UUID_FIELD, primary_key=True)
+    _id = models.CharField(max_length=UUID_FIELD)
     studyName = models.CharField(max_length=MEDIUM_LENGTH)
     kind = models.CharField(max_length=MEDIUM_LENGTH)
     data = fields.JSONField()
