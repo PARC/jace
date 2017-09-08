cf5ef1db193ec1217c730bd1f20b209e20563d30
docjoel <joelschooler@me.com>
2017-09-08 12:06:01 -0700

    added last report tracker

--- jace/settings.py
+++ jace/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import djcelery
 import os
+
+djcelery.setup_loader()
+BROKER_URL = 'pyamqp://docker.for.mac.guest@localhost:32771//'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
