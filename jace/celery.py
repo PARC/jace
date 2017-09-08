a821a2027549e2a217b4afb378c465fc01ccd5f8
docjoel <joelschooler@me.com>
2017-09-08 15:51:18 -0700

    added last report tracker

--- jace/celery.py
+++ jace/celery.py
@@ -5,7 +5,8 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jace.settings')
 
-app = Celery('jace',broker='pyamqp://docker.for.mac.localhost:32771//')
+app = Celery('jace',broker='pyamqp://docker.for.mac.localhost:32771//',backend=os.environ['REDIS'].split(':')[0],
+             port=os.environ['REDIS'])
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
