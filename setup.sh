4d3b457d042998646eaaf03c115401ae5fa5aaca
docjoel <joelschooler@me.com>
2018-01-06 11:50:35 -0800

    run changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apk update
 apk add py3-psycopg2
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py makemigrations user_model
 python manage.py migrate
