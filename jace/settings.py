2cb6721a84a7da07c6c1dbb69072b73dc0b5e8d0
docjoel <joelschooler@me.com>
2018-01-10 09:31:44 -0800

    allowed hosts

--- jace/settings.py
+++ jace/settings.py
@@ -15,7 +15,7 @@ import huey
 from huey import RedisHuey
 from redis import ConnectionPool
 
-BROKER_URL = 'pyamqp://docker.for.mac.localhost:32771//'
+# BROKER_URL = 'pyamqp://docker.for.mac.localhost:32771//'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
