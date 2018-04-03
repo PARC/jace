6784266a2000e0ff1a8b46797ada7f710ec77a93
Michael Youngblood <michael.youngblood@parc.com>
2018-04-03 12:53:51 -0700

    Update signals.py

--- user_model/signals.py
+++ user_model/signals.py
@@ -1,3 +1,21 @@
+'''
+*************************************************************************
+*
+*  © [2018] PARC Inc., A Xerox Company
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Xerox Corporation.
+* The intellectual and technical concepts contained
+* herein are proprietary to PARC Inc. and Xerox Corp.,
+* and may be covered by U.S. and Foreign Patents,
+* patents in process, and may be protected by copyright law.
+* This file is subject to the terms and conditions defined in
+* file 'LICENSE.md', which is part of this source code package.
+*
+**************************************************************************
+'''
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
