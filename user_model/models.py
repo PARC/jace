56965e861422b25d942951e0f129fef9daa6dfe5
Michael Youngblood <michael.youngblood@parc.com>
2018-04-03 12:52:33 -0700

    Update models.py

--- user_model/models.py
+++ user_model/models.py
@@ -1,4 +1,21 @@
-# Create your models here.
+'''
+*************************************************************************
+*
+*  © [2018] PARC Inc., A Xerox Company
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Xerox Corporation.
+* The intellectual and technical concepts contained
+* herein are proprietary to PARC Inc. and Xerox Corp.,
+* and may be covered by U.S. and Foreign Patents,
+* patents in process, and may be protected by copyright law.
+* This file is subject to the terms and conditions defined in
+* file 'LICENSE.md', which is part of this source code package.
+*
+**************************************************************************
+'''
+
 from django.db import models
 from django.contrib.postgres import fields
 UUID_FIELD = 36
